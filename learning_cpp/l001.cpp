3df80d9b22e9979d075acafb59b3dcadec22afa8
mistovek016 <aryaman.aisola@gmail.com>
2025-09-30 19:35:58 +0530

    learning_cpp

--- learning_cpp/l001.cpp
+++ learning_cpp/l001.cpp
@@ -2,8 +2,19 @@
 
 int main() {
     int x = 5;
+    int y = 10;
+    int sum = x + y;
+    int product = x * y;
+    int div = x / y;
+    int mod = x % y;
+
+    int n = 9.99;
+    
+    std::cout << n << std::endl;
+    std::cout << sum << " " << product << " " << div << " " << mod << std::endl;
     std::cout << "Input: ";
     std::cin >> x;
-    std::cout << "Output" << x;
+    std::cout << "Output: " << x;
+    
     return 0;
 }
