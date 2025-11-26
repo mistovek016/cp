68680a24f9ae9da507970e79512fa4ac3df62d58
mistovek016 <aryaman.aisola@gmail.com>
2025-11-26 11:06:33 +0530

    lcpp

--- learning_cpp/l001.cpp
+++ learning_cpp/l001.cpp
@@ -1,20 +1,33 @@
 #include <iostream>
 
 int main() {
-    int x = 5;
-    int y = 10;
-    int sum = x + y;
-    int product = x * y;
-    int div = x / y;
-    int mod = x % y;
+  int x;
+  x = 13;
+  int y = 123;
 
-    int n = 9.99;
-    
-    std::cout << n << std::endl;
-    std::cout << sum << " " << product << " " << div << " " << mod << std::endl;
-    std::cout << "Input: ";
-    std::cin >> x;
-    std::cout << "Output: " << x;
-    
-    return 0;
+  int sum = x + y;
+  // int days = 34.234;
+  int days = 34;
+  std::cout << x << '\n' << y << '\n';
+  std::cout << sum << '\n';
+  std::cout << days << '\n';
+
+  double price = 234.34, gpa = 29.9, temp = 64.32;
+  std::cout << price << '\n' << gpa << '\n' << temp;
+
+  // char grade = 'A', initials = 'BC', keyboard = '"df"';
+  char grade = 'A', initials = 'C', keyboard = '"';
+  std::cout << grade << '\n' << initials << '\n' << keyboard; 
+
+  bool student = false;
+  bool isSwitchOn = true;
+  bool power = true;
+  std::cout << '\n' << student << isSwitchOn << power;
+
+  std::string name = "Aryaman";
+  std::string email = "a.a@a.com";
+  std::cout << '\n' << name << '\n' << email << '\n';
+
+  std::cout << "Hello " << name << "!\n";
+  return 0;
 }
---
+++ learning_cpp/start.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Yooooo!" << std::endl;
+    std::cout << "What's up?" << std::endl;
+    return 0;
+}
